6e3f4d65b5efa918cb0b9bc654426bdf1f075ad7
Matt Yow <hello@mattyow.com>
2026-08-21 15:11:48 -0400

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import TopNav from "@/components/TopNav";
 import "./globals.css";
 import CursorBlend from "@/components/CursorBlend";
+import ExternalLinks from "@/components/ExternalLinks";
 
 export const metadata: Metadata = {
   title: {
